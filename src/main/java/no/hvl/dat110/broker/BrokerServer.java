38b4b5dc35905b01452f5b111d5d0eb8de447880
gunni95 <gunni95@outlook.com>
2024-03-09 16:26:31 +0100

    DONE WITH TASK

--- src/main/java/no/hvl/dat110/broker/BrokerServer.java
+++ src/main/java/no/hvl/dat110/broker/BrokerServer.java
@@ -4,7 +4,7 @@ import no.hvl.dat110.common.Logger;
 
 public class BrokerServer extends Thread {
 
-	private static int BROKER_DEFAULTPORT = 8080;
+	private static int BROKER_DEFAULTPORT = 8080; // DONE
 	
 	public static void main(String[] args) {
 		
